3d476031e07eacaaaef8650064b9c41ee4f06cd4
Luisa <luisa.cutillo78@gmail.com>
2020-08-27 13:21:06 +0100

    adding setup from Alexis

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='OscopeBootstrap',
+    version='0.1dev',
+    packages=['OscopeBootstrap',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
