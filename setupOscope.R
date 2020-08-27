3c8d3039a454f828635f963617a9638aeebff70b
Luisa <luisa.cutillo78@gmail.com>
2020-08-27 13:24:27 +0100

    R Setup

---
+++ setupOscope.R
@@ -0,0 +1,14 @@
+source("http://bioconductor.org/biocLite.R")
+biocLite('BiocUpgrade')
+biocLite("Oscope")
+
+install.packages("BiocInstaller",
+                 repos="http://bioconductor.org/packages/3.2/bioc")
+
+
+source("https://bioconductor.org/biocLite.R")
+
+biocLite()
+
+# see https://www.bioconductor.org/install/
+  
