ba262b5040d45e6e50efb13612fc6a9a1bc1148f
Luisa <luisa.cutillo78@gmail.com>
2020-08-27 13:22:39 +0100

    Delete CustomLibrary.R

--- CustomLibrary.R
+++
@@ -1,8 +0,0 @@
-community.significance.test <- function(graph, Nodeids, ...) {
-  if (is.directed(graph)) stop("The graph is not undirected")
-  subgraph <- induced.subgraph(graph, Nodeids)
-  in.degrees <- degree(subgraph)
-  out.degrees <- degree(graph, Nodeids) - in.degrees
-  res=wilcox.test(in.degrees, out.degrees, alternative = "greater",paired=FALSE,exact=FALSE)
-  return(res)
-}
